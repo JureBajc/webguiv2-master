62c588bee37d273c954674f95d005440aeb97f57
jure.bajc <you@example.com>
2024-07-19 08:46:21 +0200

    modified:   src/components/FixTabs.vue

--- src/components/FixTabs.vue
+++ src/components/FixTabs.vue
@@ -7,19 +7,19 @@
             <v-expansion-panels>
 
               <v-expansion-panel>
-                <v-expansion-panel-title>Acquisition Parameters</v-expansion-panel-title>
+                <v-expansion-panel-title id="colorSec">Acquisition Parameters</v-expansion-panel-title>
                 <v-expansion-panel-text>
                   <v-expansion-panels>
                     
                     <v-expansion-panel>
-                      <v-expansion-panel-title>Pre-trigger</v-expansion-panel-title>
+                      <v-expansion-panel-title id="colorThr">Pre-trigger</v-expansion-panel-title>
                       <v-expansion-panel-text>
                         testtrigger
                       </v-expansion-panel-text>
                     </v-expansion-panel>
                     
                     <v-expansion-panel>
-                      <v-expansion-panel-title>Holdoff</v-expansion-panel-title>
+                      <v-expansion-panel-title id="colorThr">Holdoff</v-expansion-panel-title>
                       <v-expansion-panel-text>
                         testholdoff
                       </v-expansion-panel-text>
@@ -30,40 +30,40 @@
               </v-expansion-panel>
 
               <v-expansion-panel>
-                <v-expansion-panel-title>Trigger settings</v-expansion-panel-title>
+                <v-expansion-panel-title id="colorSec">Trigger settings</v-expansion-panel-title>
                 <v-expansion-panel-text>
                   <v-expansion-panels>
                     
                     <v-expansion-panel>
-                      <v-expansion-panel-title>General trigger settings</v-expansion-panel-title>
+                      <v-expansion-panel-title id="colorThr">General trigger settings</v-expansion-panel-title>
                       <v-expansion-panel-text>
                         testtrigger
                       </v-expansion-panel-text>
                     </v-expansion-panel>
                     
                     <v-expansion-panel>
-                      <v-expansion-panel-title>Channel I trigger settings</v-expansion-panel-title>
+                      <v-expansion-panel-title id="colorThr">Channel I trigger settings</v-expansion-panel-title>
                       <v-expansion-panel-text>
                         testholdoff
                       </v-expansion-panel-text>
                     </v-expansion-panel>
 
                     <v-expansion-panel>
-                      <v-expansion-panel-title>Channel II trigger settings</v-expansion-panel-title>
+                      <v-expansion-panel-title id="colorThr">Channel II trigger settings</v-expansion-panel-title>
                       <v-expansion-panel-text>
                         testholdoff
                       </v-expansion-panel-text>
                     </v-expansion-panel>
 
                     <v-expansion-panel>
-                      <v-expansion-panel-title>Channel VII trigger settings</v-expansion-panel-title>
+                      <v-expansion-panel-title id="colorThr">Channel VII trigger settings</v-expansion-panel-title>
                       <v-expansion-panel-text>
                         testholdoff
                       </v-expansion-panel-text>
                     </v-expansion-panel>
 
                     <v-expansion-panel>
-                      <v-expansion-panel-title>Channel VIII trigger settings</v-expansion-panel-title>
+                      <v-expansion-panel-title id="colorThr">Channel VIII trigger settings</v-expansion-panel-title>
                       <v-expansion-panel-text>
                         testholdoff
                       </v-expansion-panel-text>
@@ -74,33 +74,33 @@
               </v-expansion-panel>
 
               <v-expansion-panel>
-                <v-expansion-panel-title>Calibration</v-expansion-panel-title>
+                <v-expansion-panel-title id="colorSec">Calibration</v-expansion-panel-title>
                 <v-expansion-panel-text>
                   <v-expansion-panels>
                     
                     <v-expansion-panel>
-                      <v-expansion-panel-title>Channel I</v-expansion-panel-title>
+                      <v-expansion-panel-title id="colorThr">Channel I</v-expansion-panel-title>
                       <v-expansion-panel-text>
                         testholdoff
                       </v-expansion-panel-text>
                     </v-expansion-panel>
 
                     <v-expansion-panel>
-                      <v-expansion-panel-title>Channel II</v-expansion-panel-title>
+                      <v-expansion-panel-title id="colorThr">Channel II</v-expansion-panel-title>
                       <v-expansion-panel-text>
                         testholdoff
                       </v-expansion-panel-text>
                     </v-expansion-panel>
 
                     <v-expansion-panel>
-                      <v-expansion-panel-title>Channel VII</v-expansion-panel-title>
+                      <v-expansion-panel-title id="colorThr">Channel VII</v-expansion-panel-title>
                       <v-expansion-panel-text>
                         testholdoff
                       </v-expansion-panel-text>
                     </v-expansion-panel>
 
                     <v-expansion-panel>
-                      <v-expansion-panel-title>Channel VIII</v-expansion-panel-title>
+                      <v-expansion-panel-title id="colorThr">Channel VIII</v-expansion-panel-title>
                       <v-expansion-panel-text>
                         testholdoff
                       </v-expansion-panel-text>
@@ -111,7 +111,7 @@
               </v-expansion-panel>
 
               <v-expansion-panel>
-                <v-expansion-panel-title>Input coupling</v-expansion-panel-title>
+                <v-expansion-panel-title id="colorSec">Input coupling</v-expansion-panel-title>
                 <v-expansion-panel-text>
                   Cinputcoup
                 </v-expansion-panel-text>
@@ -127,7 +127,7 @@
 
             <v-expansion-panels>
               <v-expansion-panel>
-                <v-expansion-panel-title>Accelerometer</v-expansion-panel-title>
+                <v-expansion-panel-title id="colorSec">Accelerometer</v-expansion-panel-title>
                 <v-expansion-panel-text>
                   accelltest
                 </v-expansion-panel-text>
@@ -152,10 +152,16 @@ export default {
 </script>
   
   <style scoped>
+  #colorThr{
+    background-color:#0f93cd;
+  }
+  #colorSec{
+    background-color:#1a5d7c;
+  }
   .v-expansion-panel-title {
     font-weight: bold;
     color: white;
-    background-color: #12142a;
+    background-color: #081d27;
   }
   .custom-width {
     max-width: 600px;
